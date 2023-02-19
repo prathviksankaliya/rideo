d208ad55b4f327b8b988deaf26a99d33d1243a6f
Shadow <prathviksankaliya@gmail.com>
2023-02-19 11:35:49 +0530

    google sign in Done

--- app/src/main/java/com/itcraftsolution/raido/Fragments/LoginFragment.java
+++ app/src/main/java/com/itcraftsolution/raido/Fragments/LoginFragment.java
@@ -1,25 +1,52 @@
 package com.itcraftsolution.raido.Fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.itcraftsolution.raido.R;
 import com.itcraftsolution.raido.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
 
     private FragmentLoginBinding binding;
+    private GoogleSignInClient googleSignInClient;
+    private ActivityResultLauncher<Intent> signInActivityLauncher;
+    private FirebaseAuth auth;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(getLayoutInflater());
+
+        createGoogleRequest();
+        auth = FirebaseAuth.getInstance();
 
         binding.btnLoginOtp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +55,56 @@ public class LoginFragment extends Fragment {
                         new LoginProfileFragment()).addToBackStack(null).commit();
             }
         });
+
+        binding.btnGoogleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInWithGoogle();
+            }
+        });
+
+        signInActivityLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                    try{
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        signInWithGoogleAccount(account);
+                    }catch (ApiException a)
+                    {
+
+                    }
+                }
+            }
+        });
         return binding.getRoot();
+    }
+
+    private void createGoogleRequest() {
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.firebaseClientId))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), googleSignInOptions);
+    }
+
+    private void signInWithGoogle() {
+        Intent intent = googleSignInClient.getSignInIntent();
+        signInActivityLauncher.launch(intent);
+    }
+
+    private void signInWithGoogleAccount(GoogleSignInAccount account){
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        auth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(requireContext(), "Email: "+ account.getEmail(), Toast.LENGTH_SHORT).show();
+                    }
+            }
+        });
     }
 }
